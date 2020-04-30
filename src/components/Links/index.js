32d3409c557a53612940c00e05db92a1af3428c9
talitagaldino <talita.gouveia@ccc.ufcg.edu.br>
2020-04-30 13:23:27 -0300

    update 3

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './styles.css';
+import foto from './talita.png'
+import fota from './tali3.png'
+import fotos from './lita.jpg'
 
 const Links = () => {
     return (
         <div className={'caixaLinks'}>
-           <a href='https://www.google.com/' target="_blank">taliiiiiiiiiii</a> 
+
+            <a href='https://www.google.com/' target="_blank"> 
+                <img className={'links'} src={foto}></img>
+            </a>
+
+            <a href='https://www.google.com/' target="_blank"> 
+                <img className={'links'} src={fotos}></img>    
+            </a>
+            
+            <a href='https://www.google.com/' target="_blank"> 
+                <img className={'links'} src={fota}></img>    
+            </a>
+
         </div>
     );
 }
