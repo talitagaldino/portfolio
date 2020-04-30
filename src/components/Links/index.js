782c255f5a16c5c4086987d9677663ced5bded8a
talitagaldino <talita.gouveia@ccc.ufcg.edu.br>
2020-04-30 14:21:56 -0300

    update 4

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './styles.css';
-import foto from './talita.png'
-import fota from './tali3.png'
-import fotos from './lita.jpg'
+import git from './github.png'
+import linkedin from './linkedin.png'
+import insta from './insta.png'
 
 const Links = () => {
     return (
         <div className={'caixaLinks'}>
 
             <a href='https://www.google.com/' target="_blank"> 
-                <img className={'links'} src={foto}></img>
+                <img className={'links'} src={git}></img>
             </a>
 
-            <a href='https://www.google.com/' target="_blank"> 
-                <img className={'links'} src={fotos}></img>    
+            <a href='https://www.facebook.com/' target="_blank"> 
+                <img className={'links'} src={linkedin}></img>   
             </a>
             
-            <a href='https://www.google.com/' target="_blank"> 
-                <img className={'links'} src={fota}></img>    
+            <a href='https://www.instagram.com/' target="_blank"> 
+                <img className={'links'} src={insta}></img>    
             </a>
 
         </div>
