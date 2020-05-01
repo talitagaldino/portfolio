3d10fc3c4eb19664a4bdd66edee59e77765628c1
talitagaldino <talita.gouveia@ccc.ufcg.edu.br>
2020-04-30 22:19:06 -0300

    Updates

---
+++ precache-manifest.28833cc1d9daf09c42adbeb6c92f1610.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e002e80a01414fd911ba45121e53b366",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bd7737ea4741ae7468",
+    "url": "/static/css/main.66ab3217.chunk.css"
+  },
+  {
+    "revision": "214d948457ccbed7527b",
+    "url": "/static/js/2.dd7e55f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd7e55f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2bd7737ea4741ae7468",
+    "url": "/static/js/main.e45757e5.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "318728f6b5fa0271fbc1f838972a3762",
+    "url": "/static/media/github.318728f6.png"
+  },
+  {
+    "revision": "d2ad0db47894cd81b9395df18c52ff88",
+    "url": "/static/media/insta.d2ad0db4.png"
+  },
+  {
+    "revision": "fa52c1c9de9c3d7f3314ee3b1d505fc0",
+    "url": "/static/media/linkedin.fa52c1c9.png"
+  },
+  {
+    "revision": "fa61cc26c8158414bf2b1765ae2c5282",
+    "url": "/static/media/talita.fa61cc26.png"
+  }
+]);
