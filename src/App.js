e2afd210a609ecc02e4c2bedf6eff7e9718589a9
Talita Galdino <talita.gouveia@ccc.ufcg.edu.br>
2020-07-21 22:13:59 -0300

    Add header

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Descricao from './components/Descricao';
 import Links from './components/Links';
 import Informacoes from './components/Informacoes';
 import Rodape from './components/Rodape';
+import Header from './components/Header';
 
 const App = () => (
   <div>  
     <div className={'caixaExterna'}>
         <NavBar />
       <div className={'caixaInterna'}> 
+        <Header />
         <img src={foto} className={'imagem'}></img>
         <Titulo />
         <Descricao />
---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.css';
+import foto from './desenho.png';
+
+const Header = () => {
+    return(
+
+        <div className="container">
+            <section id="home">
+                <div className="container_conjunto">
+                    <div className="container_imagem">
+                        <img src={foto} width="100%"/>
+                    </div>
+                    <div className="container_titulo">
+                        <h2> Talita Galdino </h2>
+                        <h1> Computer science student </h1>
+                    </div>
+                    <div className="container_links">
+                        <h1> teste links </h1>
+                    </div>
+                </div> 
+            </section>
+        </div>
+    
+    );
+};
+
+export default Header;
